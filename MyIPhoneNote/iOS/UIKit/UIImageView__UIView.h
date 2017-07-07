c09b6d17e41bcd491983414c21c0f2ca3fd3c53a
JingshunZhang <jingshun.zhang@asiainnovations.com>
2017-07-07 09:41:16 +0800

    UIView修改

--- MyIPhoneNote/iOS/UIKit/UIImageView__UIView.h
+++ MyIPhoneNote/iOS/UIKit/UIImageView__UIView.h
@@ -171,5 +171,33 @@ rotationAnimation.cumulative = YES;
 rotationAnimation.repeatCount = 1;
 
 [self.refreshBtn.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
-11,设置图片的填充模式
+11,设置图片的填充模式 contentMode
+UIViewContentModeScaleToFill
+
+UIViewContentModeScaleAspectFit
+
+UIViewContentModeScaleAspectFill
+
+UIViewContentModeRedraw
+
+UIViewContentModeCenter
+
+UIViewContentModeTop
+
+UIViewContentModeBottom
+
+UIViewContentModeLeft
+
+UIViewContentModeRight
+
+UIViewContentModeTopLeft
+
+UIViewContentModeTopRight
+
+UIViewContentModeBottomLeft
+
+UIViewContentModeBottomRight
+
+注意以上几个常量，凡是没有带Scale的，当图片尺寸超过
+ImageView尺寸时，只有部分显示在ImageView中。UIViewContentModeScaleAspectFit会保证图片比例不变，而且全部显示在ImageView中，这意味着ImageView会有部分空白。UIViewContentModeScaleAspectFill也会证图片比例不变，但是是填充整个ImageView的，可能只有部分图片显示出来。
 
